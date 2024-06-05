91075e40ae6079ec458d1598abab30a5874aa997
Luke Bakken <luke@bakken.io>
2024-06-05 09:07:29 -0700

    Cluster works

---
+++ consumer/Program.cs
@@ -0,0 +1,133 @@
+﻿using System.Diagnostics;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+
+const string exchangeName = "status_exchange";
+const string queueName = "status_updates";
+
+AutoResetEvent latch = new AutoResetEvent(false);
+
+void CancelHandler(object? sender, ConsoleCancelEventArgs e)
+{
+    Console.WriteLine("CTRL-C pressed, exiting!");
+    e.Cancel = true;
+    latch.Set();
+}
+
+Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelHandler);
+
+string hostName = "rmq0.local";
+ushort port = 5672;
+
+Console.WriteLine($"CONSUMER: waiting 10 seconds to try initial connection to {hostName}:{port}");
+if (latch.WaitOne(TimeSpan.FromSeconds(10)))
+{
+    Console.WriteLine("CONSUMER EXITING");
+    Environment.Exit(0);
+}
+
+var factory = new ConnectionFactory()
+{
+    HostName = hostName,
+    Port = port,
+    AutomaticRecoveryEnabled = false,
+    TopologyRecoveryEnabled = false
+};
+
+bool connected = false;
+
+IConnection? connection = null;
+
+while(!connected)
+{
+    try
+    {
+        connection = factory.CreateConnection();
+        connected = true;
+    }
+    catch (BrokerUnreachableException)
+    {
+        connected = false;
+        Console.WriteLine("CONSUMER: waiting 5 seconds to re-try connection!");
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+    }
+}
+
+int message_count = 0;
+
+using (connection)
+{
+    if (connection == null)
+    {
+        Console.Error.WriteLine("CONSUMER: unexpected null connection");
+    }
+    else
+    {
+        connection.CallbackException += (s, ea) =>
+        {
+            var cea = (CallbackExceptionEventArgs)ea;
+            Console.Error.WriteLine($"CONSUMER: connection.CallbackException: {cea}");
+        };
+
+        connection.ConnectionBlocked += (s, ea) =>
+        {
+            var cbea = (ConnectionBlockedEventArgs)ea;
+            Console.Error.WriteLine($"CONSUMER: connection.ConnectionBlocked: {cbea}");
+        };
+
+        connection.ConnectionUnblocked += (s, ea) =>
+        {
+            Console.Error.WriteLine($"CONSUMER: connection.ConnectionUnblocked: {ea}");
+        };
+
+        connection.ConnectionShutdown += (s, ea) =>
+        {
+            var sdea = (ShutdownEventArgs)ea;
+            Console.Error.WriteLine($"CONSUMER: connection.ConnectionShutdown: {sdea}");
+        };
+
+        using (var channel = connection.CreateModel())
+        {
+            channel.CallbackException += (s, ea) =>
+            {
+                var cea = (CallbackExceptionEventArgs)ea;
+                Console.Error.WriteLine($"CONSUMER: channel.CallbackException: {cea}");
+            };
+
+            channel.ModelShutdown += (s, ea) =>
+            {
+                var sdea = (ShutdownEventArgs)ea;
+                Console.Error.WriteLine($"CONSUMER: channel.ModelShutdown: {sdea}");
+            };
+
+            channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Topic, durable: true);
+
+            var queueDeclareResult = channel.QueueDeclare(queue: queueName, durable: true,
+                    exclusive: false, autoDelete: false, arguments: null);
+            Debug.Assert(queueName == queueDeclareResult.QueueName);
+
+            channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: "update.*");
+
+            channel.BasicQos(0, 1, false);
+
+            Console.WriteLine("CONSUMER: waiting for messages...");
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                DateTime received = DateTime.Now;
+                string receivedText = received.ToString("MM/dd/yyyy HH:mm:ss.ffffff");
+                Console.WriteLine($"CONSUMER received at {receivedText}, size {ea.Body.Length}, message_count: {message_count++}");
+                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+
+            channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+
+            latch.WaitOne();
+
+            Console.WriteLine("CONSUMER EXITING");
+        }
+    }
+}
---
+++ producer/Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Diagnostics;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+
+const string exchangeName = "status_exchange";
+const string queueName = "status_updates";
+const string hostName = "rmq1.local";
+const ushort port = 5672;
+
+AutoResetEvent latch = new AutoResetEvent(false);
+
+void CancelHandler(object? sender, ConsoleCancelEventArgs e)
+{
+    Console.WriteLine("CTRL-C pressed, exiting!");
+    e.Cancel = true;
+    latch.Set();
+}
+
+Console.CancelKeyPress += new ConsoleCancelEventHandler(CancelHandler);
+
+Console.WriteLine($"PRODUCER: waiting 10 seconds to try initial connection to {hostName}:{port}");
+Thread.Sleep(TimeSpan.FromSeconds(10));
+
+var factory = new ConnectionFactory()
+{
+    HostName = hostName,
+    Port = port
+};
+
+TimeSpan latchWaitSpan = TimeSpan.FromSeconds(1);
+bool connected = false;
+
+IConnection? connection = null;
+
+while (!connected)
+{
+    try
+    {
+        connection = factory.CreateConnection();
+        connected = true;
+    }
+    catch (BrokerUnreachableException)
+    {
+        connected = false;
+        Console.WriteLine($"PRODUCER: waiting 5 seconds to re-try connection!");
+        Thread.Sleep(TimeSpan.FromSeconds(5));
+    }
+}
+
+byte[] buffer = new byte[1024];
+Random rnd = new Random();
+
+using (connection)
+{
+    if (connection == null)
+    {
+        Console.Error.WriteLine("PRODUCER: unexpected null connection");
+    }
+    else
+    {
+        connection.CallbackException += (s, ea) =>
+        {
+            var cea = (CallbackExceptionEventArgs)ea;
+            Console.Error.WriteLine($"PRODUCER: connection.CallbackException: {cea}");
+        };
+
+        connection.ConnectionBlocked += (s, ea) =>
+        {
+            var cbea = (ConnectionBlockedEventArgs)ea;
+            Console.Error.WriteLine($"PRODUCER: connection.ConnectionBlocked: {cbea}");
+        };
+
+        connection.ConnectionUnblocked += (s, ea) =>
+        {
+            Console.Error.WriteLine($"PRODUCER: connection.ConnectionUnblocked: {ea}");
+        };
+
+        connection.ConnectionShutdown += (s, ea) =>
+        {
+            var sdea = (ShutdownEventArgs)ea;
+            Console.Error.WriteLine($"PRODUCER: connection.ConnectionShutdown: {sdea}");
+        };
+
+        using (var channel = connection.CreateModel())
+        {
+            channel.CallbackException += (s, ea) =>
+            {
+                var cea = (CallbackExceptionEventArgs)ea;
+                Console.Error.WriteLine($"PRODUCER: channel.CallbackException: {cea}");
+            };
+
+            channel.ModelShutdown += (s, ea) =>
+            {
+                var sdea = (ShutdownEventArgs)ea;
+                Console.Error.WriteLine($"PRODUCER: channel.ModelShutdown: {sdea}");
+            };
+
+            channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Topic, durable: true);
+
+            var queueDeclareResult = channel.QueueDeclare(queue: queueName, durable: true,
+                    exclusive: false, autoDelete: false, arguments: null);
+            Debug.Assert(queueName == queueDeclareResult.QueueName);
+
+            channel.QueueBind(queue: queueName, exchange: exchangeName, routingKey: "update.*");
+
+            channel.ConfirmSelect();
+
+            var latchSpan = TimeSpan.FromSeconds(1);
+            while (false == latch.WaitOne(latchSpan))
+            {
+                rnd.NextBytes(buffer);
+                string now = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss.ffffff");
+                string routingKey = string.Format("update.{0}", Guid.NewGuid().ToString());
+                channel.BasicPublish(exchange: exchangeName, routingKey: routingKey, basicProperties: null, body: buffer);
+                channel.WaitForConfirmsOrDie();
+                Console.WriteLine($"PRODUCER sent message at {now}");
+            }
+        }
+    }
+}
